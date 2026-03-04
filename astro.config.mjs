39c98fac8e32b00b278f177c1dfed6f31928d79b
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2026-03-04 14:08:08 -0500

    feat: assemble landing page

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://yilmerguativa.github.io',
+  base: '/elite-paws/',
+  build: {
+    format: 'file'
+  }
+});
