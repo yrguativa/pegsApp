ad829a2c4e7bd9f14ea1d592500b3b59b9f57300
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2026-03-04 14:29:04 -0500

    feat: implement new design with Tailwind CSS and i18n support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://yrguativa.github.io',
   base: '/pegsApp/',
+
   build: {
     format: 'file'
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
